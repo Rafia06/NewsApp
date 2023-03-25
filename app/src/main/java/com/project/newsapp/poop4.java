a543ea68469daf16550409e8b6ee8d0a68949125
rafia34 <shahrin1907034@stud.kuet.ac.bd>
2023-03-25 21:45:09 +0600

    file added

---
+++ app/src/main/java/com/project/newsapp/poop4.java
@@ -0,0 +1,17 @@
+package com.project.newsapp;
+
+public class poop4 {
+    String poo6;
+
+    public poop4(String poo6) {
+        this.poo6 = poo6;
+    }
+
+    public String getPoo6() {
+        return poo6;
+    }
+
+    public void setPoo6(String poo6) {
+        this.poo6 = poo6;
+    }
+}
