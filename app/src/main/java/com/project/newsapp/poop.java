54459ff1e5a2ca2b14c162cc7df4c65fc3dfa046
rafia34 <shahrin1907034@stud.kuet.ac.bd>
2023-03-25 21:29:56 +0600

    file added

---
+++ app/src/main/java/com/project/newsapp/poop.java
@@ -0,0 +1,16 @@
+package com.project.newsapp;
+
+public class poop {
+    String poop1;
+    public poop(String poop1) {
+        this.poop1 = poop1;
+    }
+
+    public String getPoop1() {
+        return poop1;
+    }
+
+    public void setPoop1(String poop1) {
+        this.poop1 = poop1;
+    }
+}
