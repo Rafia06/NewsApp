51856ec77d98ab01785b4f6795c6a0586d8da83b
rafia <shahrin1907034@stud.kuet.ac.bd>
2023-03-26 06:02:32 +0600

    file added

---
+++ app/src/main/java/com/project/newsapp/lab13.java
@@ -0,0 +1,17 @@
+package com.project.newsapp;
+
+public class lab13 {
+    String lab21;
+
+    public lab13(String lab21) {
+        this.lab21 = lab21;
+    }
+
+    public String getLab21() {
+        return lab21;
+    }
+
+    public void setLab21(String lab21) {
+        this.lab21 = lab21;
+    }
+}
